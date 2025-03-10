c60d55f14bcccf86ae2fa5788cf5003a761841b9
lombarde1 <milenakurst@gmail.com>
2025-03-09 23:06:37 -0300

    att

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://api.horizon777.online';
+const API_BASE_URL = 'https://horizon777api-production.up.railway.app';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
